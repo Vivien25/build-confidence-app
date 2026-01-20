56c6ba759d6d1a1c1eb9e44dc0c1c01f12742cae
You Li <youlee2504@gmail.com>
2026-01-19 21:59:54 -0600

    change app name and add Kai

--- frontend/src/utils/avatars.js
+++ frontend/src/utils/avatars.js
@@ -1,7 +1,9 @@
+import miraImg from "../assets/coach_mira.png";
+import kaiImg from "../assets/coach_kai.png";
+
 import userFem from "../assets/avatars/user_fem.png";
 import userMasc from "../assets/avatars/user_masc.png";
 import userNeutral from "../assets/avatars/user_neutral.png";
-import coachPng from "../assets/avatars/coach.png";
 
 export const avatarMap = {
   fem: { label: "Feminine", img: userFem },
@@ -9,4 +11,8 @@ export const avatarMap = {
   neutral: { label: "Neutral", img: userNeutral },
 };
 
-export const coachAvatar = { label: "Coach", img: coachPng };
+export const COACHES = {
+  mira: { id: "mira", name: "Mira", avatar: miraImg },
+  kai: { id: "kai", name: "Kai", avatar: kaiImg },
+};
+
