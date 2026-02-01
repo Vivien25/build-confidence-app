153f30353e153c64d843d658040a4007acd00b5a
You Li <youlee2504@gmail.com>
2026-02-01 15:19:56 -0600

    home UI enhancement

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -176,7 +176,7 @@ const styles = {
     /* 🔪 crop to left 66% 
     clipPath: "inset(0 15% 0 0)",*/
   
-    transform: "scale(1.05) translateX(34%)", // ✅ more right
+    transform: "scale(1.05) translateX(24%)", // ✅ more right
     transformOrigin: "center",
   
     filter: "drop-shadow(0 18px 30px rgba(0,0,0,0.15))",
